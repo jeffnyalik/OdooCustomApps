214e0ae129de9cd5427ab59159ebddda6ab6eb80
jeffnyak@gmail.com <jeffnyak@gmail.com>
2022-03-09 00:13:21 +0300

    Initial Commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+### Custom Hotel Management Module
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'sequence': -100,
+    'category': 'Medicine',
+    'author': 'Jeff Nyak',
+    'summary': 'Hospital Management System for Clinics and Pharmacies',
+    'description': """Odoo Module for Doctors,Clinicians, Nurses etc""",
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+
+}
